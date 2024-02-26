ad48ce1f63012b79e08a6cd9188b8348a6c1a184
Drew <drewdavey22@gmail.com>
2024-02-26 12:19:04 -0800

    nn

--- CODE/numFrames.py
+++ CODE/numFrames.py
@@ -60,7 +60,8 @@ def run(path0,path1,pathLog,num_frames,dt):
 	outfile.write('\n' + 'Stop Time: ' + datetime.utcnow().strftime('%H%M%S%f')[:-3] + '\n')
 	outfile.write('Done collecting images.' + '\n')
 	outfile.close()
-
+	cam0.close()
+	cam1.close()
 	exit()
 		
 
