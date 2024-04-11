baadede5cd40d5273c08337d35c7afd65d68c1ac
Drew <drewdavey22@gmail.com>
2024-04-10 17:55:59 -0700

    back to 1920x1080

--- CODE/numFrames.py
+++ CODE/numFrames.py
@@ -20,8 +20,8 @@ def run(path0,path1,pathLog,num_frames,dt):
 	config1 = cam1.create_still_configuration()
 	
 	# Configuration settings
-	config0['main']['size'] = (1500, 1000)  # Resolution for cam0
-	config1['main']['size'] = (1500, 1000)  # Resolution for cam1
+	config0['main']['size'] = (1920, 1080)  # Resolution for cam0
+	config1['main']['size'] = (1920, 1080)  # Resolution for cam1
 	config0['controls']['FrameDurationLimits'] = (33333, 33333)  # Frame rate (in microseconds) for cam0
 	config1['controls']['FrameDurationLimits'] = (33333, 33333)  # Frame rate for cam1
 	
