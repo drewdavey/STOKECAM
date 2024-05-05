c0974c5aec18d91cb6a6c8fec8e222c6fac7ad1f
Drew <drewdavey22@gmail.com>
2024-05-05 15:50:54 -0700

    added post process scripts

---
+++ post_processing/calibrate.m
@@ -0,0 +1,64 @@
+% Stereo Calibration Code
+% Drew Davey
+% Last updated: 2024-03-05 
+
+clear; clc; close all;
+
+rectifiedImagesDir = 'Rectified_Images'; 
+if ~exist(rectifiedImagesDir, 'dir')
+    mkdir(rectifiedImagesDir); % mkdir for rectified images
+end
+
+dir1 = dir('cam0/*.jpg');
+dir2 = dir('cam1/*.jpg');
+
+% Check the number of files in each directory
+numFiles = min(length(dir1), length(dir2));
+
+% Initialize arrays to store file paths
+imageFileNames1 = cell(numFiles, 1);
+imageFileNames2 = cell(numFiles, 1);
+
+% Construct file paths for each directory
+for i = 1:numFiles
+    imageFileNames1{i} = fullfile(dir1(i).folder, dir1(i).name);
+    imageFileNames2{i} = fullfile(dir2(i).folder, dir2(i).name);
+end
+
+% Detect calibration pattern in images
+detector = vision.calibration.stereo.CheckerboardDetector();
+[imagePoints, imagesUsed] = detectPatternPoints(detector, imageFileNames1, imageFileNames2);
+
+% Generate world coordinates for the planar patten keypoints
+squareSize = 45;  % in units of 'millimeters'
+worldPoints = generateWorldPoints(detector, 'SquareSize', squareSize);
+
+% Read one of the images from the first stereo pair
+I1 = imread(imageFileNames1{1});
+[mrows, ncols, ~] = size(I1);
+
+% Calibrate the camera
+[stereoParams, pairsUsed, estimationErrors] = estimateCameraParameters(imagePoints, worldPoints, ...
+    'EstimateSkew', true, 'EstimateTangentialDistortion', true, ...
+    'NumRadialDistortionCoefficients', 3, 'WorldUnits', 'millimeters', ...
+    'InitialIntrinsicMatrix', [], 'InitialRadialDistortion', [], ...
+    'ImageSize', [mrows, ncols]);
+
+% View reprojection errors
+h1=figure; showReprojectionErrors(stereoParams);
+
+% Visualize pattern locations
+h2=figure; showExtrinsics(stereoParams, 'CameraCentric');
+
+% Display parameter estimation errors
+displayErrors(estimationErrors, stereoParams);
+
+% You can use the calibration data to rectify stereo images.
+I2 = imread(imageFileNames2{1});
+[J1, J2, reprojectionMatrix] = rectifyStereoImages(I1, I2, stereoParams);
+
+
+%% Add to default 
+
+clearvars -except stereoParams
+save("calib.mat");
