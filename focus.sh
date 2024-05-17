490196086f86bc23b1c45663d0ab8c0366d2a6a1
Drew <drewdavey22@gmail.com>
2024-05-17 11:46:40 -0700

    added focus.sh

---
+++ focus.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Last updated: 2024-04-10
+##################################
+# This script creates two preview windows to focus the cameras
+# Inputs: (1) Width (default = 1440) (2) Height (default = 1080)
+# Example:
+#  ./focus.sh 1440 1080
+##################################
+ 
+# Parse command line input
+if [ $# -eq 0 ]; then
+  width=1440
+  height=1080
+elif [ $# -eq 1 ]; then
+  width=$1
+  height=1080
+else
+  width=$1
+  height=$2
+fi
+
+# Two windows live stream
+libcamera-hello --width $width --height $height -p 0,0,$width,$height --camera 0 -t 0 & libcamera-hello --width $width --height $height -p 0,0,$width,$height --camera 1 -t 0
