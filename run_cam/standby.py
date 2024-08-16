9f9019c7646542e0b972e11d935a88ed870b3af6
Drew <drewdavey22@gmail.com>
2024-08-16 15:58:18 -0700

    get stoked

--- run_cam/standby.py
+++ run_cam/standby.py
@@ -4,10 +4,10 @@ from gpiozero import Button
 from subprocess import check_call
 from signal import pause
 
-def shutdown():
+def get_stoked():
     check_call(['sudo', 'poweroff'])
 
 stoke_on = Button(17, hold_time=5)
-stoke_on.when_held = stoke_on
+stoke_on.when_held = get_stoked
 
 pause()
