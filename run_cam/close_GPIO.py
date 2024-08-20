c2ba1e1329ad70bc0998c9184c96ef0c47b5bab1
Drew <drewdavey22@gmail.com>
2024-08-20 15:15:18 -0700

    gg

--- run_cam/close_GPIO.py
+++ run_cam/close_GPIO.py
@@ -1,6 +1,3 @@
 import gpiozero.pins.rpigpio
 
-def close(self): pass
-gpiozero.pins.rpigpio.RPiGPIOPin.close = close
-
-gpiozero.LED(..., pin_factory=gpiozero.pins.rpigpio.RPiGPIOFactory())
+gpiozero.pins.rpigpio.RPiGPIOPin.close 
