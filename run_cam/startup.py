4035716cca2f5e0d1c32c83ee539be1f23fe0c3e
Drew <drewdavey22@gmail.com>
2024-08-27 16:58:00 -0700

    try gainj

--- run_cam/startup.py
+++ run_cam/startup.py
@@ -31,13 +31,11 @@ def configure_cameras(log):
     # Apply minimal settings to both cameras
     for cam in [cam0, cam1]:
         print(config)
-        camera_config = cam.create_still_configuration(
-            main={"size": config['resolution'], "format": "RGB888"},
-        )
-        cam.configure(camera_config)
+        camera_config = cam.create_still_configuration(config)
+        cam.configure("still")
 
         cam.start()
-        log.write(f"Minimal Camera configuration: {cam.camera_configuration()}\n")
+        log.write(f"Still Camera configuration: {cam.camera_configuration()}\n")
 
 
 #         #### pull each camera config and print to log ################'
