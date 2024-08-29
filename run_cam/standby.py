b0b1f9df7f1271cb11c3be96a53ecc07192cead6
Drew <drewdavey22@gmail.com>
2024-08-29 15:22:40 -0700

    print

--- run_cam/standby.py
+++ run_cam/standby.py
@@ -39,7 +39,7 @@ def configure_cameras(log):
         config = cam.create_still_configuration()
 
         for key, value in settings['config'].items():
-            exec(f"{key} = {value}")
+            print(f"{key} = {value}")
         # for setting in settings['config']:
         #     config[setting] = settings['config'][setting]
             # Handle tuple conversion for lists
