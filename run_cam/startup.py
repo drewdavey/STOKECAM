08b0c108e56700b397e4930fcd3eae03a8f395a7
Drew <drewdavey22@gmail.com>
2024-08-30 13:24:23 -0700

    cs

--- run_cam/startup.py
+++ run_cam/startup.py
@@ -37,13 +37,13 @@ def sync_clock_and_imu(pathLog):
     # at the baud rate of 115200 (115,200 bytes/s) 
     s = VnSensor()
     s.connect('/dev/ttyUSB0', 115200)
-    comp = CompositeData.connect('/dev/ttyUSB0', 115200)
+    comp = s.CompositeData
 
     with open(pathLog, 'a') as log:
         model_num = s.read_model_number()
         serial_num = s.read_serial_number()
         log.write(f"Connected to VN-200: Model {model_num}, Serial: {serial_num}\n")
-        
+
         time_utc = comp.time_utc
         log.write(f"Time from VN-200: {time_utc}\n")
 
