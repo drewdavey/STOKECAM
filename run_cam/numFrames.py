c739a5217629dec82446bef6829970ea96a93924
Drew <drewdavey22@gmail.com>
2024-09-01 01:05:46 -0700

    done

--- run_cam/numFrames.py
+++ run_cam/numFrames.py
@@ -20,7 +20,9 @@ def run(fdir_cam0,fdir_cam1,fname_log,fname_imu,num_frames,dt):
 		cam.start()
 		log.write(f"cam{idx} configuration: {cam.camera_configuration()}\n")
 		log.write(f"cam{idx} metadata: {cam.capture_metadata()}\n")
+
 	imu_process = subprocess.Popen(['python3', 'imu.py', fname_imu, fname_log])
+	
 	for i in range(int(num_frames)):
 		tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3] 
 		cam0.capture_file(f"{fdir_cam0}0_{tstr}_{i+1:05}.jpg")
