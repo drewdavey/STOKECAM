ec877a320b89b808c84341d3c06d92171ba9f942
Drew <drewdavey22@gmail.com>
2024-09-01 00:31:14 -0700

    s

--- run_cam/test.py
+++ run_cam/test.py
@@ -9,8 +9,8 @@ from standby import create_dirs
 
 config = get_still_configuration() # get still config from settings.py. add statement here to choose mode
 
-cam0 = Picamera2(0)
-cam1 = Picamera2(1)
+cam = Picamera2(0)
+# cam1 = Picamera2(1)
 
 # cam0.stop()
 # cam1.stop()
