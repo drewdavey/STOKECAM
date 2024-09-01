92bfae2a8d102d8df5dab3a4771a0c8b4018c346
Drew <drewdavey22@gmail.com>
2024-08-31 18:21:50 -0700

    cc

--- run_cam/startup.py
+++ run_cam/startup.py
@@ -46,11 +46,11 @@ def sync_clock_and_imu(pathLog):
         model_num = s.read_model_number()
         serial_num = s.read_serial_number()
         vn_pos = s.read_gps_solution_lla()
-        vn_time = ez.current_data
+        vn_time = ez.current_data.time_utc
 
         log.write(f"Connected to VN-200: Model {model_num}, Serial: {serial_num}\n")
         log.write(f"Current position (LLA): ({vn_pos.lla.x}, {vn_pos.lla.y}, {vn_pos.lla.z})\n")
-        log.write(f"Time from VN-200: {dir(vn_time)}\n")
+        log.write(f"Time from VN-200: {vn_time}\n")
 
     # while True:
     #     gps_data = gps_serial.readline().decode('ascii', errors='replace')
