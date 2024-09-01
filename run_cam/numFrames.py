d2a12ae3df5db1f385ae81fe2b29a3411aa4d77c
Drew <drewdavey22@gmail.com>
2024-09-01 00:12:06 -0700

    x

--- run_cam/numFrames.py
+++ run_cam/numFrames.py
@@ -2,13 +2,13 @@
 
 import time
 import subprocess
-from settings import *
+# from settings import *
 from picamera2 import Picamera2
 from datetime import datetime, timezone
 from startup import setup_logging, read_inputs_yaml
 from standby import create_dirs
 
-config = get_still_configuration() # get still config from settings.py. add statement here to choose mode
+# config = get_still_configuration() # get still config from settings.py. add statement here to choose mode
 
 cam0 = Picamera2(0)
 cam1 = Picamera2(1)
