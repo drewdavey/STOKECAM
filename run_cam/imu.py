806e9d0ef4dcbff969199f443d03b37c8db582d2
Drew <drewdavey22@gmail.com>
2024-08-31 19:08:22 -0700

    nn

--- run_cam/imu.py
+++ run_cam/imu.py
@@ -11,11 +11,9 @@ import psutil
 # Import vnpy library
 from vnpy import *
 
-# Create sensor object and connect to the VN-200 
-# at the baud rate of 115200 (115,200 bytes/s)
-s = VnSensor()
-s.connect('/dev/ttyUSB0', 115200)
-
+# Connect to the VN-200 
+ez = EzAsyncData.connect('/dev/ttyUSB0', 115200)
+s = ez.sensor                                       # create sensor object for VN-200
 
 # Record the start time
 # start_time = time.time()
@@ -31,21 +29,21 @@ def is_pid_active(pid):
     
 def run_imu(log_path,imu_path,imu_dt,cam_pid):
 
-    imu_out = open(imu_path, 'a')
+    imu = open(imu_path, 'a')
     log_out = open(log_path, 'a')
         
-    imu_out.write(str('Time(UTC),Heading(deg, magN),Yaw(deg),Roll(deg),Pitch(deg)') + '\n')
+    imu.write(imu_headerLine = "Timestamp (UTC: HHMMSSsss), VN-200 Timestamp (UTC), Temperature (deg C), Pressure (Pa), Yaw (deg), Pitch (deg), Roll (deg), Accel_x, Accel_y, Accel_z, Gyro_x, Gyro_y, Gyro_z, Mag_x, Mag_y, Mag_z, GPS_LLA, INS_LLA\n")
 
     while is_pid_active(cam_pid):
 
-        # Read yaw, pitch, and roll values
-        ypr = s.read_yaw_pitch_roll()
-        
-        # Print the yaw, pitch, and roll values
-        imu_out.write(f"Yaw: {ypr.x}, Pitch: {ypr.y}, Roll: {ypr.z}" + '\n')
-        
-        reg = s.read_yaw_pitch_roll_magnetic_acceleration_and_angular_rates()
-        imu_out.write(f"Acc_X: {reg.accel.x}, Acc_Y: {reg.accel.y}, Acc_Z: {reg.accel.z}" + '\n')
+        ####################### IMU #######################
+        ypr = s.read_yaw_pitch_roll() # Read yaw, pitch, and roll values
+        gps = s.read_gps_solution_lla() # Read the GPS solution in LLA format
+        ins = s.read_ins_solution_lla() # Read the INS solution
+        imu_out = s.read_imu_measurements() # Read the IMU measurements
+        cd = ez.current_data # Read current data as CompositeData class from the EzAsyncData
+        imu.write(f"{tstr}, {cd.time_utc}, {cd.temperature} or {imu_out.temp}, {cd.pressure} or {imu_out.pressure}, {ypr.x}, {ypr.y}, {ypr.z}, {imu_out.accel.x}, {imu_out.accel.y}, {imu_out.accel.z}, {imu_out.gyro.x}, {imu_out.gyro.y}, {imu_out.gyro.z}, {imu_out.mag.x}, {imu_out.mag.y}, {imu_out.mag.z}, ({gps.lla.x}, {gps.lla.y}, {gps.lla.z}), ({ins.position.x}, {ins.position.y}, {ins.position.z})" + '\n')
+        ###################################################
 
         # Pause for imu_dt seconds
         time.sleep(imu_dt)  
--- run_cam/standby.py
+++ run_cam/standby.py
@@ -85,18 +85,6 @@ def numFrames(fdir, log, dt, num_frames):
     imu.close()
     busy = False
 
-# def write_imu(fname_imu, tstr):
-#     imu = open(fname_imu, 'a')
-#     imu.write(imu_headerLine)  # Header line
-#     ####################### IMU #######################
-#     ypr = s.read_yaw_pitch_roll() # Read yaw, pitch, and roll values
-#     gps = s.read_gps_solution_lla() # Read the GPS solution in LLA format
-#     ins = s.read_ins_solution_lla() # Read the INS solution
-#     imu_out = s.read_imu_measurements() # Read the IMU measurements
-#     cd = ez.current_data # Read current data as CompositeData class from the EzAsyncData
-#     imu.write(f"{tstr}, {cd.time_utc}, {cd.temperature} or {imu_out.temp}, {cd.pressure} or {imu_out.pressure}, {ypr.x}, {ypr.y}, {ypr.z}, {imu_out.accel.x}, {imu_out.accel.y}, {imu_out.accel.z}, {imu_out.gyro.x}, {imu_out.gyro.y}, {imu_out.gyro.z}, {imu_out.mag.x}, {imu_out.mag.y}, {imu_out.mag.z}, ({gps.lla.x}, {gps.lla.y}, {gps.lla.z}), ({ins.position.x}, {ins.position.y}, {ins.position.z})" + '\n')
-#     ###################################################
-
 def create_dirs(fdir, mode):
     session = datetime.now(timezone.utc).strftime('%H%M%S_' + mode)
     fdir_out = os.path.join(fdir, session + '/')
