67faadb2a181f84b86fa479e21987eb9e5830faa
Drew <drewdavey22@gmail.com>
2024-09-01 00:30:09 -0700

    x

--- run_cam/test.py
+++ run_cam/test.py
@@ -1,10 +1,18 @@
 
-from picamera2 import Picamera2
+import time
+import subprocess
 from settings import *
+from picamera2 import Picamera2
+from datetime import datetime, timezone
+from startup import setup_logging, read_inputs_yaml
+from standby import create_dirs
 
-config = get_still_configuration() 
+config = get_still_configuration() # get still config from settings.py. add statement here to choose mode
 
-cam = Picamera2(0)
-cam.start()
-cam.stop()
-cam.close()
+cam0 = Picamera2(0)
+cam1 = Picamera2(1)
+
+cam0.stop()
+cam1.stop()
+cam0.close()
+cam1.close()
