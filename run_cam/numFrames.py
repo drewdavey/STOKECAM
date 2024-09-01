dccaddfb46b0db7acfdb205f7a31fc2e3220d09a
Drew <drewdavey22@gmail.com>
2024-08-31 22:05:47 -0700

    cc

--- run_cam/numFrames.py
+++ run_cam/numFrames.py
@@ -10,8 +10,6 @@ from settings import *
 from startup import setup_logging, read_inputs_yaml
 from standby import create_dirs
 
-config = get_still_configuration() # get still config from settings.py. add statement here to choose mode
-
 # Connect to the cameras
 cam0 = Picamera2(0)
 cam1 = Picamera2(1)
@@ -42,6 +40,6 @@ fdir, fname_log = setup_logging()               # Setup logging
 inputs = read_inputs_yaml(fname_log)            # Read inputs from inputs.yaml
 num_frames = inputs['num_frames']
 dt = inputs['dt']
-
+config = get_still_configuration() # get still config from settings.py. add statement here to choose mode
 fdir_out, fdir_cam0, fdir_cam1, fname_imu = create_dirs(fdir, 'numFrames')
 run(fdir_cam0,fdir_cam1,fname_log,num_frames,dt)
