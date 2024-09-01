b44b6736565dde1256a16d605c4fcf622742992c
Drew <drewdavey22@gmail.com>
2024-09-01 00:49:11 -0700

    x

--- run_cam/numFrames.py
+++ run_cam/numFrames.py
@@ -1,5 +1,5 @@
 # Last updated: 2024-09-01
-
+import os
 import time
 import subprocess
 time.sleep(1)
@@ -9,7 +9,7 @@ from picamera2 import Picamera2
 time.sleep(1)
 from datetime import datetime, timezone
 from startup import setup_logging, read_inputs_yaml
-from standby import create_dirs
+# from standby import create_dirs
 
 config = get_still_configuration() # get still config from settings.py. add statement here to choose mode
 time.sleep(1)
@@ -38,9 +38,24 @@ def run(fdir_cam0,fdir_cam1,fname_log,fname_imu,num_frames,dt):
 	cam1.close()
 	exit() 
 
+def create_dirs(fdir, mode):
+    session = datetime.now(timezone.utc).strftime('%H%M%S_' + mode)
+    fdir_out = os.path.join(fdir, session + '/')
+    fdir_cam0 = os.path.join(fdir_out, 'cam0/')
+    fdir_cam1 = os.path.join(fdir_out, 'cam1/')
+    os.makedirs(fdir_cam0, exist_ok=True)
+    os.makedirs(fdir_cam1, exist_ok=True)
+    fname_imu = f'{fdir_out}IMU_{session}.txt'
+    print(f'--Created output folders: {fdir_cam0} and {fdir_cam1}')
+    return fdir_out, fdir_cam0, fdir_cam1, fname_imu
+
 fdir, fname_log = setup_logging()               # Setup logging
 inputs = read_inputs_yaml(fname_log)            # Read inputs from inputs.yaml
 num_frames = inputs['num_frames']
 dt = inputs['dt']
+
 fdir_out, fdir_cam0, fdir_cam1, fname_imu = create_dirs(fdir, 'numFrames')
+
+
+
 run(fdir_cam0, fdir_cam1, fname_log, fname_imu, num_frames, dt)
