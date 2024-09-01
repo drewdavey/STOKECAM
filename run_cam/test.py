7a395705522588b60d6b53b99f9c8c1ba4af2b95
Drew <drewdavey22@gmail.com>
2024-09-01 00:30:42 -0700

    s

--- run_cam/test.py
+++ run_cam/test.py
@@ -12,7 +12,7 @@ config = get_still_configuration() # get still config from settings.py. add stat
 cam0 = Picamera2(0)
 cam1 = Picamera2(1)
 
-cam0.stop()
-cam1.stop()
-cam0.close()
-cam1.close()
+# cam0.stop()
+# cam1.stop()
+# cam0.close()
+# cam1.close()
