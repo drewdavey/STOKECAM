5e54479e5812e9ddb7d099a11c08c4f28a7ceb26
Drew <drewdavey22@gmail.com>
2024-09-01 17:20:11 -0700

    x

--- run_cam/test.py
+++ run_cam/test.py
@@ -1,7 +1,7 @@
 
 import time
 from vnpy import *
-
+from datetime import datetime, timezone
 
 # Create sensor object and connect to the VN-200 
 # at the baud rate of 115200 (115,200 bytes/s) 
@@ -13,37 +13,45 @@ s = ez.sensor
 model_num = s.read_model_number()
 serial_num = s.read_serial_number()
 
+fname_log = 'test_LOG.txt'
+log = open(fname_log, 'a')
+
 while not ez.current_data.has_fix:
 
-    print('#####################################################')
+    log.write('#####################################################')
+
+    tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3]
+    log.write(f"{tstr}\n")
+    
     pos = ez.current_data.has_any_position
-    print('Has position: ', str(pos), "\n")
+    log.write('Has position: ', str(pos), "\n")
 
     hasposun = ez.current_data.has_any_position_uncertainty
-    print('Has position uncertainty: ', str(hasposun), "\n")
+    log.write('Has position uncertainty: ', str(hasposun), "\n")
 
     posun = ez.current_data.any_position_uncertainty
-    print('Position uncertainty: ', str(posun), "\n")
+    log.write('Position uncertainty: ', str(posun), "\n")
 
     posunes = ez.current_data.position_uncertainty_estimated
-    print('Position uncertainty estimate: ', str(posunes), "\n")
+    log.write('Position uncertainty estimate: ', str(posunes), "\n")
 
     gpstime = ez.current_data.has_time_gps
-    print('Has GPS time: ', str(gpstime), "\n")
+    log.write('Has GPS time: ', str(gpstime), "\n")
 
     vn_pos = s.read_gps_solution_lla()
-    print('GPS Pos: ', vn_pos.lla, '\n')
+    log.write('GPS Pos: ', vn_pos.lla, '\n')
 
     vn_time = ez.current_data.time_utc
-    print('GPS Time: ', vn_time, '\n')
+    log.write('GPS Time: ', vn_time, '\n')
 
     num_sats = ez.current_data.num_sats
-    print("Number of satellites: ", str(num_sats), "\n")
+    log.write("Number of satellites: ", str(num_sats), "\n")
 
     has_num_sats = ez.current_data.has_num_sats
-    print("Has Number of satellites: ", str(has_num_sats), "\n")
+    log.write("Has Number of satellites: ", str(has_num_sats), "\n")
 
-    print('#####################################################')
+    log.write('#####################################################')
     time.sleep(2)
 
+log.close()
 s.disconnect()
