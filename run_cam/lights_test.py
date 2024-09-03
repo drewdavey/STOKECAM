e717b08ce006ce609d916d7a8d003d2424b3471c
Drew <drewdavey22@gmail.com>
2024-09-02 18:13:30 -0700

    s

--- run_cam/lights_test.py
+++ run_cam/lights_test.py
@@ -5,17 +5,21 @@ from time import sleep
 
 right_button = Button(18, hold_time=3)  
 left_button = Button(17, hold_time=3)
+right_button.close()
+left_button.close()
+right_button = Button(18, hold_time=3)  
+left_button = Button(17, hold_time=3)
 
 green = LED(12)
 
 green.on()
 
-while True:
-    if (right_button.is_held and left_button.is_held): 
-        right_button.close()
-        left_button.close() 
-        process = subprocess.Popen(['python3', 'sub_lights.py'])
-        process.wait()
-        right_button = Button(18, hold_time=3)  
-        left_button = Button(17, hold_time=3)
-        sleep(1)
+# while True:
+#     if (right_button.is_held and left_button.is_held): 
+#         right_button.close()
+#         left_button.close() 
+#         process = subprocess.Popen(['python3', 'sub_lights.py'])
+#         process.wait()
+#         right_button = Button(18, hold_time=3)  
+#         left_button = Button(17, hold_time=3)
+#         sleep(1)
