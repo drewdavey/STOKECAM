0d0252b2322f43e79da85077852c76a75c03143b
Drew <drewdavey22@gmail.com>
2024-09-02 18:29:25 -0700

    d

--- run_cam/lights_test.py
+++ run_cam/lights_test.py
@@ -16,17 +16,17 @@ left_button = Button(17, hold_time=3)
 
 green = LED(12)
 
-# green.on()
+green.on()
 
-# while True:
-#     if (right_button.is_held and left_button.is_held): 
-#         right_button.close()
-#         left_button.close() 
-#         process = subprocess.Popen(['python3', 'sub_lights.py'])
-#         process.wait()
-#         right_button = Button(18, hold_time=3)  
-#         left_button = Button(17, hold_time=3)
-#         sleep(1)
+while True:
+    if (right_button.is_held and left_button.is_held): 
+        right_button.close()
+        left_button.close() 
+        process = subprocess.Popen(['python3', 'sub_lights.py'])
+        process.wait()
+        right_button = Button(18, hold_time=3)  
+        left_button = Button(17, hold_time=3)
+        sleep(1)
 
 
 # if (right_button.is_held and left_button.is_held): 
@@ -37,53 +37,3 @@ green = LED(12)
 #     right_button = Button(18, hold_time=3)  
 #     left_button = Button(17, hold_time=3)
 #     sleep(1)
-
-standby = None
-def start_subprocess():
-    global standby, left_button, right_button
-    if standby is None:
-        # Close buttons to free up GPIO pins
-        left_button.close()
-        right_button.close()
-        
-        # Start child process
-        standby = Popen(['python', 'sub_lights.py'])
-        sleep(0.5)  # Debounce delay
-
-        # Reinitialize buttons after subprocess starts
-        left_button = Button(17)
-        right_button = Button(18)
-        left_button.when_pressed = stop_subprocess
-        right_button.when_pressed = start_subprocess
-
-def stop_subprocess():
-    global standby, left_button, right_button
-    if standby is not None:
-        standby.send_signal(SIGINT)
-        standby.wait()
-
-        # Close and reinitialize buttons after subprocess ends
-        left_button.close()
-        right_button.close()
-        left_button = Button(17)
-        right_button = Button(18)
-        left_button.when_pressed = stop_subprocess
-        right_button.when_pressed = start_subprocess
-        
-        standby = None
-        sleep(0.5)  # Debounce delay
-
-left_button.when_held = start_subprocess
-right_button.when_pressed = stop_subprocess
-
-def monitor_condition():
-    while True:
-        # Placeholder for condition to control green light
-        green.on()
-        sleep(1)  # Check every second
-
-try:
-    monitor_condition()
-except KeyboardInterrupt:
-    # Everything will clean up itself due to gpiozero's built-in cleanup
-    pass
--- run_cam/sub_lights.py
+++ run_cam/sub_lights.py
@@ -3,33 +3,34 @@ import sys
 import time
 from gpiozero import Button, LED
 
-time.sleep(1)
 yellow = LED(16)
 red = LED(26)
-right_button = Button(18, hold_time=3)  # 
-left_button = Button(17, hold_time=3)   # 
+# right_button = Button(18, hold_time=3)  # 
+# left_button = Button(17, hold_time=3)   # 
 
-def exit_standby():
-    red.close()
-    yellow.close() # Close the lights
-    right_button.close() 
-    left_button.close() # Close the buttons
-    sys.exit(0)
+yellow.on()
+red.on()    
+# def exit_standby():
+#     red.close()
+#     yellow.close() # Close the lights
+#     right_button.close() 
+#     left_button.close() # Close the buttons
+#     sys.exit(0)
 
-def standby():
+# def standby():
 
-    yellow.on()
+#     yellow.on()
 
-    while not (right_button.is_held and left_button.is_held):
+#     while not (right_button.is_held and left_button.is_held):
 
-        while right_button.is_pressed and not left_button.is_pressed:
-            red.on()
-        while left_button.is_pressed and not right_button.is_pressed:
-            red.on()
-        red.off()
-        time.sleep(0.2)
+#         while right_button.is_pressed and not left_button.is_pressed:
+#             red.on()
+#         while left_button.is_pressed and not right_button.is_pressed:
+#             red.on()
+#         red.off()
+#         time.sleep(0.2)
 
-    exit_standby()
+#     exit_standby()
 
-if __name__ == "__main__":
-    standby()  
+# if __name__ == "__main__":
+#     standby()  
