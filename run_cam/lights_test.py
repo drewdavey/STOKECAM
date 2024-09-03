e70a1005f23388c78cfb255c117dfe67b97b2259
Drew <drewdavey22@gmail.com>
2024-09-02 17:05:35 -0700

    x

--- run_cam/lights_test.py
+++ run_cam/lights_test.py
@@ -1,4 +1,4 @@
-
+import time
 from picamera2 import Picamera2
 from gpiozero import Button, LED
 from signal import pause
@@ -12,13 +12,26 @@ green = LED(12)
 
 green.on()
 
-process = subprocess.Popen(['python3', 'sub_lights.py'])
 
-while not process.poll() is None:  # Check if standby.py is still running
-    right_button = Button(18, hold_time=5) 
-    left_button = Button(17, hold_time=5)
-    if (right_button.is_held and left_button.is_held):
+# while not process.poll() is None:  # Check if standby.py is still running
+#     right_button = Button(18, hold_time=5) 
+#     left_button = Button(17, hold_time=5)
+#     if (right_button.is_held and left_button.is_held):
+#         green.close()
+#         right_button.close()
+#         left_button.close()
+#         exit()
+
+right_button = Button(18, hold_time=3)  
+left_button = Button(17, hold_time=3)
+
+while True:
+    if (right_button.is_held and left_button.is_held): 
         green.close()
         right_button.close()
-        left_button.close()
-        exit()
+        left_button.close() 
+        process = subprocess.Popen(['python3', 'sub_lights.py'])
+        process.wait()
+        right_button = Button(18, hold_time=3)  
+        left_button = Button(17, hold_time=3)
+        time.sleep(0.2)
