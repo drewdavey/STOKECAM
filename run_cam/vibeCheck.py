f667d39d80387ebcbe0953c520da81064937223f
Drew <drewdavey22@gmail.com>
2024-09-03 16:12:18 -0700

    stop cams only

--- run_cam/vibeCheck.py
+++ run_cam/vibeCheck.py
@@ -20,63 +20,27 @@ red = LED(26)
 right_button = Button(18, hold_time=3)  # 
 left_button = Button(17, hold_time=3)   # 
 
-# # Connect to the cameras
-# cam0 = Picamera2(0)
-# cam1 = Picamera2(1)
+# Connect to the cameras
+cam0 = Picamera2(0)
+cam1 = Picamera2(1)
 
 busy = False
 
 def configure_cameras(fname_log, config):
     tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3]
     log = open(fname_log, 'a')
-    # Connect to the cameras
-    cam0 = Picamera2(0)
-    cam1 = Picamera2(1)
+    # # Connect to the cameras
+    # cam0 = Picamera2(0)
+    # cam1 = Picamera2(1)
     for idx, cam in enumerate([cam0, cam1]):
+        cam.stop()
         cam.configure(config)
         cam.start()
         log.write(f"{tstr}:     cam{idx} configuration: {cam.camera_configuration()}\n")
         log.write(f"{tstr}:     cam{idx} metadata: {cam.capture_metadata()}\n")
     log.close()
-    return cam0, cam1
+    # return cam0, cam1
 
-# def burst(fdir, fname_log, dt): 
-#     global busy
-#     i = 0
-#     fdir_out, fdir_cam0, fdir_cam1, fname_imu = create_dirs(fdir, 'burst')
-#     imu_process = subprocess.Popen(['python3', 'imu.py', fname_imu, fname_log])
-#     log = open(fname_log, 'a')
-#     tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3]
-#     log.write(f"{tstr}:     burst session: {fdir_out}\n")
-#     while right_button.is_pressed:
-#         red.on()
-#         tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3]
-#         cam0.capture_file(f"{fdir_cam0}0_{tstr}_{i+1:05}.jpg")
-#         cam1.capture_file(f"{fdir_cam1}1_{tstr}_{i+1:05}.jpg")
-#         i += 1
-#         red.off()
-#         time.sleep(dt)
-#     imu_process.terminate()
-#     busy = False
-#     log.close()
-
-# def numFrames(fdir, fname_log, dt, num_frames):
-#     global busy
-#     fdir_out, fdir_cam0, fdir_cam1, fname_imu = create_dirs(fdir, 'numFrames')
-#     imu_process = subprocess.Popen(['python3', 'imu.py', fname_imu, fname_log])
-#     log = open(fname_log, 'a')
-#     tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3]
-#     log.write(f"{tstr}:     numFrames session: {fdir_out}\n")
-#     for i in range(int(num_frames)):
-#         red.on()
-#         tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3] 
-#         cam0.capture_file(f"{fdir_cam0}0_{tstr}_{i+1:05}.jpg")
-#         cam1.capture_file(f"{fdir_cam1}1_{tstr}_{i+1:05}.jpg")
-#         red.off()
-#         time.sleep(dt)
-#     imu_process.terminate()
-#     busy = False
-#     log.close()
 def shoot_mode0(fdir, fname_log, dt, mode0): 
     global busy
     i = 0
@@ -96,8 +60,8 @@ def shoot_mode0(fdir, fname_log, dt, mode0):
         red.off()
         time.sleep(dt)
     imu_process.terminate()
-    cam0.close() # Stop the cameras
-    cam1.close() 
+    # cam0.close() # Stop the cameras
+    # cam1.close() 
     busy = False
     log.close()
 
@@ -111,7 +75,7 @@ def shoot_mode1(fdir, fname_log, dt, mode1):
     log.write(f"{tstr}:     {mode1} session: {fdir_out}\n")
     config = get_config(mode1)
     cam0, cam1 = configure_cameras(fname_log, config)            # Configure the cameras
-    while right_button.is_pressed:
+    while left_button.is_pressed:
         red.on()
         tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3]
         cam0.capture_file(f"{fdir_cam0}0_{tstr}_{i+1:05}.jpg")
@@ -120,8 +84,8 @@ def shoot_mode1(fdir, fname_log, dt, mode1):
         red.off()
         time.sleep(dt)
     imu_process.terminate()
-    cam0.close() # Stop the cameras
-    cam1.close() 
+    # cam0.close() # Stop the cameras
+    # cam1.close() 
     busy = False
     log.close()
 
@@ -149,8 +113,8 @@ def calib(fdir, fname_log, calib_dt, calib_frames, calib_mode):
         [led.off() for led in (red, green, yellow)]
         time.sleep(calib_dt)
     imu_process.terminate()
-    cam0.close() # Stop the cameras
-    cam1.close() 
+    # cam0.close() # Stop the cameras
+    # cam1.close() 
     busy = False
     log.close()
 
@@ -227,8 +191,8 @@ while not (right_button.is_held and left_button.is_held):
 ########################################################
 
 ##################### Cleanup ########################## 
-# cam0.close() # Close the cameras
-# cam1.close()
+cam0.close() # Close the cameras
+cam1.close()
 green.close()
 yellow.close() # Close the LEDs
 red.close() 
@@ -236,3 +200,41 @@ right_button.close()
 left_button.close() # Close the buttons
 sys.exit(0)
 ########################################################
+
+# def burst(fdir, fname_log, dt): 
+#     global busy
+#     i = 0
+#     fdir_out, fdir_cam0, fdir_cam1, fname_imu = create_dirs(fdir, 'burst')
+#     imu_process = subprocess.Popen(['python3', 'imu.py', fname_imu, fname_log])
+#     log = open(fname_log, 'a')
+#     tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3]
+#     log.write(f"{tstr}:     burst session: {fdir_out}\n")
+#     while right_button.is_pressed:
+#         red.on()
+#         tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3]
+#         cam0.capture_file(f"{fdir_cam0}0_{tstr}_{i+1:05}.jpg")
+#         cam1.capture_file(f"{fdir_cam1}1_{tstr}_{i+1:05}.jpg")
+#         i += 1
+#         red.off()
+#         time.sleep(dt)
+#     imu_process.terminate()
+#     busy = False
+#     log.close()
+
+# def numFrames(fdir, fname_log, dt, num_frames):
+#     global busy
+#     fdir_out, fdir_cam0, fdir_cam1, fname_imu = create_dirs(fdir, 'numFrames')
+#     imu_process = subprocess.Popen(['python3', 'imu.py', fname_imu, fname_log])
+#     log = open(fname_log, 'a')
+#     tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3]
+#     log.write(f"{tstr}:     numFrames session: {fdir_out}\n")
+#     for i in range(int(num_frames)):
+#         red.on()
+#         tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')[:-3] 
+#         cam0.capture_file(f"{fdir_cam0}0_{tstr}_{i+1:05}.jpg")
+#         cam1.capture_file(f"{fdir_cam1}1_{tstr}_{i+1:05}.jpg")
+#         red.off()
+#         time.sleep(dt)
+#     imu_process.terminate()
+#     busy = False
+#     log.close()
