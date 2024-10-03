60a51d40003af295e5805258cdb81807ceb50a5e
Drew <drewdavey22@gmail.com>
2024-10-03 15:46:52 -0700

    vv

--- run_cam/master.py
+++ run_cam/master.py
@@ -119,7 +119,7 @@ def enter_standby(fdir, fname_log, dt, mode):
     log = open(fname_log, 'a')
     tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')
     log.write(f"{tstr}:     Entering standby... \n\n"), log.close()
-    fdir_out, fdir_cam0, fdir_cam1, fname_imu = create_dirs(fdir, f"_session_{mode}")
+    fdir_out, fdir_cam0, fdir_cam1, fname_imu = create_dirs(fdir, f"session_{mode}")
     imu_process = subprocess.Popen(['python3', 'imu.py', fname_imu, fname_log])
     time.sleep(0.5)
     while not (right_button.is_held and left_button.is_held): # Hold both buttons for 3 seconds to exit standby
