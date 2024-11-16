ff87b5eef47f25351a0fda0e0b8fbf9aa3ef0405
Drew <drewdavey22@gmail.com>
2024-11-15 16:25:11 -0800

    xx

--- run_cam/run_imu/test.py
+++ run_cam/run_imu/test.py
@@ -19,26 +19,26 @@ print(dir(s))# cd = CompositeData()
 
 
 # Record the start time
-start_time = time.time()
-duration = 30  # seconds
+# start_time = time.time()
+# duration = 30  # seconds
 
-while time.time() - start_time < duration:
-    # Read yaw, pitch, and roll values
-    ypr = s.read_yaw_pitch_roll()
-    print(f"Yaw: {ypr.x}, Pitch: {ypr.y}, Roll: {ypr.z}"+ '\n')
+# while time.time() - start_time < duration:
+#     # Read yaw, pitch, and roll values
+#     ypr = s.read_yaw_pitch_roll()
+#     print(f"Yaw: {ypr.x}, Pitch: {ypr.y}, Roll: {ypr.z}"+ '\n')
 
-    gps = s.read_gps_solution_lla() # Read the GPS solution in LLA format
+#     gps = s.read_gps_solution_lla() # Read the GPS solution in LLA format
 
-    reg = s.read_yaw_pitch_roll_magnetic_acceleration_and_angular_rates()
-    print(f"Acc_X: {reg.accel.x}, Acc_Y: {reg.accel.y}, Acc_Z: {reg.accel.z}"+ '\n')
+#     reg = s.read_yaw_pitch_roll_magnetic_acceleration_and_angular_rates()
+#     print(f"Acc_X: {reg.accel.x}, Acc_Y: {reg.accel.y}, Acc_Z: {reg.accel.z}"+ '\n')
 
-    print(f" {gps.num_sats}, {gps.time}, ({gps.lla.x}, {gps.lla.y}, {gps.lla.z})" + '\n')
+#     print(f" {gps.num_sats}, {gps.time}, ({gps.lla.x}, {gps.lla.y}, {gps.lla.z})" + '\n')
 
-    # print(f"Time: {cd.time_utc}" + '\n')
-    # print(f"position_gps_lla: {cd.position_gps_lla}" + '\n')
+#     # print(f"Time: {cd.time_utc}" + '\n')
+#     # print(f"position_gps_lla: {cd.position_gps_lla}" + '\n')
 
-    # Pause for a short time to avoid flooding the command window
-    time.sleep(0.1)  
+#     # Pause for a short time to avoid flooding the command window
+#     time.sleep(0.1)  
 
 # Disconnect from the sensor
 s.disconnect()
