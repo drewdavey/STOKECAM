e88d17f40529b4f6f30d6b15e6e6f7b5b60bfaac
Drew <drewdavey22@gmail.com>
2024-11-25 16:08:52 -0800

    x

--- run_cam/utils.py
+++ run_cam/utils.py
@@ -106,7 +106,7 @@ def sync_clock_and_imu(fname_log, gps_wait_time):
         gnss = Registers.GnssSolLla()
         s.readRegister(gnss)
         print(f"GNSS Fix:  {gnss.gnss1Fix}")
-        print(f"GNSS Fix:  {gnss.gnss1TimeUtc}")
+        print(f"GNSS Num Sats:  {gnss.gnss1NumSats}")
         # while ez.current_data.position_uncertainty_estimated > 10:
         #     # num_sats = ez.current_data.num_sats
         #     # log.write(f"Number of satellites: {num_sats}\n")
