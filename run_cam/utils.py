1b70d08c34e63031ca06c61571c072123fc8a279
Drew <drewdavey22@gmail.com>
2024-11-29 21:02:22 -0800

    xx

--- run_cam/utils.py
+++ run_cam/utils.py
@@ -122,7 +122,7 @@ def sync_clock(portName, clock_timeout):
         gnssFix = gnss.gnss1Fix.name
         if gnssFix in valid_fixes:
             break
-        time.sleep(0.1)
+        time.sleep(1)
     else:
         s.disconnect()
         return False    # Sync failed
@@ -136,7 +136,7 @@ def sync_clock(portName, clock_timeout):
             vn_time = f"20{tUtc.year:02}-{tUtc.month:02}-{tUtc.day:02} {tUtc.hour:02}:{tUtc.minute:02}:{tUtc.second:02}.{tUtc.fracSec:03}"
             os.system(f"sudo date -s '{vn_time}'") # Set the system time
             # os.system("sudo hwclock --systohc")    # Sync the hardware clock
-        time.sleep(0.1)
+            time.sleep(1)
         # Check time lag between RP and VN-200
         cd = s.getNextMeasurement()
         rp_time = datetime.now(timezone.utc)
@@ -154,10 +154,10 @@ def sync_clock(portName, clock_timeout):
             diff_time = vn_time - rp_time
             diff_seconds = abs(diff_time.total_seconds())
             # Check if the time difference is < 1ms
-            if diff_seconds < 0.01:
+            if diff_seconds < 0.1:
                 s.disconnect()
                 return True  # Sync successful
-        time.sleep(0.1)  
+        time.sleep(1)  
     s.disconnect()
     return False  # Sync failed
 
