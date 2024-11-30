aed09415d3e210789298b9de0080bec3bd8ae94c
Drew <drewdavey22@gmail.com>
2024-11-29 20:47:43 -0800

    back to vn_200 format

--- run_cam/utils.py
+++ run_cam/utils.py
@@ -129,7 +129,7 @@ def sync_clock(portName, clock_timeout):
     # Sync the RP clock to the VN-200
     t0 = time.time()
     while (time.time() - t0 < clock_timeout):
-        cd = s.getNextMeasurement(1)
+        cd = s.getNextMeasurement()
         if not cd: continue
         if tUtc := cd.time.timeUtc:
             # Format the time as 'YYYY-MM-DD HH:MM:SS.fff'
@@ -137,19 +137,19 @@ def sync_clock(portName, clock_timeout):
             os.system(f"sudo date -s '{vn_time}'") # Set the system time
             os.system("sudo hwclock --systohc")    # Sync the hardware clock
         # Check time lag between RP and VN-200
-        cd = s.getNextMeasurement(1)
+        cd = s.getNextMeasurement()
         rp_time = datetime.now(timezone.utc)
         if not cd: continue
         if tUtc := cd.time.timeUtc:
-            vn_time = datetime(
-                year=2000 + tUtc.year,  # VN-200 time is 2000 + YY
-                month=tUtc.month,
-                day=tUtc.day,
-                hour=tUtc.hour,
-                minute=tUtc.minute,
-                second=tUtc.second,
-                microsecond=tUtc.fracSec * 1000,
-                tzinfo=timezone.utc)
+            vn_time = f"20{tUtc.year:02}{tUtc.month:02}{tUtc.day:02}{tUtc.hour:02}{tUtc.minute:02}{tUtc.second:02}{tUtc.fracSec:03}"
+            year = int(vn_time[:4])
+            month = int(vn_time[4:6])
+            day = int(vn_time[6:8])
+            hours = int(vn_time[8:10])
+            minutes = int(vn_time[10:12])
+            seconds = int(vn_time[12:14])
+            milliseconds = int(vn_time[14:])
+            vn_time = datetime(year, month, day, hours, minutes, seconds, milliseconds * 1000, tzinfo=timezone.utc)
             diff_time = vn_time - rp_time
             diff_seconds = abs(diff_time.total_seconds())
             # Check if the time difference is < 1ms
@@ -219,20 +219,20 @@ def VN200_status(portName, fname_log, gps_timeout):
             log.write(f"{tstr}:     GNSS Fix:  {gnssFix}, Number of satellites: {num_sats}\n")
             t0 = time.time()
             while (time.time() - t0 < gps_timeout):
-                cd = s.getNextMeasurement(1)
+                cd = s.getNextMeasurement()
                 rp_time = datetime.now(timezone.utc)
                 if not cd: continue
                 if tUtc := cd.time.timeUtc:
                     # Format the time as 'HHMMSSfff'
-                    vn_time = datetime(
-                    year=2000 + tUtc.year,  # VN-200 time is 2000 + YY
-                    month=tUtc.month,
-                    day=tUtc.day,
-                    hour=tUtc.hour,
-                    minute=tUtc.minute,
-                    second=tUtc.second,
-                    microsecond=tUtc.fracSec * 1000,
-                    tzinfo=timezone.utc)
+                    vn_time = f"20{tUtc.year:02}{tUtc.month:02}{tUtc.day:02}{tUtc.hour:02}{tUtc.minute:02}{tUtc.second:02}{tUtc.fracSec:03}"
+                    year = int(vn_time[:4])
+                    month = int(vn_time[4:6])
+                    day = int(vn_time[6:8])
+                    hours = int(vn_time[8:10])
+                    minutes = int(vn_time[10:12])
+                    seconds = int(vn_time[12:14])
+                    milliseconds = int(vn_time[14:])
+                    vn_time = datetime(year, month, day, hours, minutes, seconds, milliseconds * 1000, tzinfo=timezone.utc)
                     diff_time = vn_time - rp_time
                     diff_seconds = diff_time.total_seconds()
             tstr = datetime.now(timezone.utc).strftime('%H%M%S%f')
