6ed1d90250e22c467a851a60c3e4d8a3e4c76ec8
Drew <drewdavey22@gmail.com>
2024-12-16 08:37:38 -0800

    added quat2rotm to funcs, working on F_Analyze

---
+++ post_processing/MATLAB/functions/quat2rotm.m
@@ -0,0 +1,14 @@
+% STOKECAM Post-Processing
+% Drew Davey
+% Last updated: 2024-12-13
+
+
+function R = quat2rotm(q)
+    qw = q(1);
+    qx = q(2);
+    qy = q(3);
+    qz = q(4);
+    R = [1 - 2*qy^2 - 2*qz^2, 2*qx*qy - 2*qz*qw, 2*qx*qz + 2*qy*qw;
+         2*qx*qy + 2*qz*qw, 1 - 2*qx^2 - 2*qz^2, 2*qy*qz - 2*qx*qw;
+         2*qx*qz - 2*qy*qw, 2*qy*qz + 2*qx*qw, 1 - 2*qx^2 - 2*qy^2];
+end
