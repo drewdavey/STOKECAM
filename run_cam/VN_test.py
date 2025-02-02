d4d75886594d3dd2d46046f3de7c04d9bc0865f9
Drew <drewdavey22@gmail.com>
2025-02-01 20:40:46 -0800

    xx

--- run_cam/VN_test.py
+++ run_cam/VN_test.py
@@ -33,39 +33,39 @@ s.autoConnect(portName)           # at the baud rate of 115200 (115,200 bytes/s)
 # asyncDataOutputType.ador = Registers.AsyncOutputType.Ador.YPR
 # asyncDataOutputType.serialPort = Registers.AsyncOutputType.SerialPort.Serial1
 # # s.writeRegister(asyncDataOutputType)
-asyncDataOutputFreq= Registers.AsyncOutputFreq()
-asyncDataOutputFreq.adof = Registers.AsyncOutputFreq.Adof.Rate40Hz
-asyncDataOutputFreq.serialPort = Registers.AsyncOutputFreq.SerialPort.Serial1
-s.writeRegister(asyncDataOutputFreq)
+# asyncDataOutputFreq= Registers.AsyncOutputFreq()
+# asyncDataOutputFreq.adof = Registers.AsyncOutputFreq.Adof.Rate40Hz
+# asyncDataOutputFreq.serialPort = Registers.AsyncOutputFreq.SerialPort.Serial1
+# s.writeRegister(asyncDataOutputFreq)
 
 #### CONFIGURE THE BINARY OUTPUT
 binaryOutput1Register = Registers.BinaryOutput1()
 binaryOutput1Register.rateDivisor = 20           ### 40 Hz (~800/rateDivisor) ###
 binaryOutput1Register.asyncMode.serial1 = 1
-binaryOutput1Register.asyncMode.serial2 = 0
-binaryOutput1Register.time.timeUtc = 1
-binaryOutput1Register.time.timeStartup = 1
-binaryOutput1Register.time.timeGps = 1
-binaryOutput1Register.time.timeSyncIn = 1
-binaryOutput1Register.time.timeGpsPps = 1
-binaryOutput1Register.imu.imuStatus = 1
-binaryOutput1Register.imu.temperature = 1
-binaryOutput1Register.imu.pressure = 1
-binaryOutput1Register.imu.accel = 1
-binaryOutput1Register.imu.mag = 1
-binaryOutput1Register.attitude.ypr = 1
-binaryOutput1Register.attitude.quaternion = 1
-binaryOutput1Register.attitude.dcm = 0
-binaryOutput1Register.ins.posLla = 1
-binaryOutput1Register.ins.posEcef = 1
-binaryOutput1Register.ins.posU = 1
-binaryOutput1Register.ins.insStatus = 1
-binaryOutput1Register.gnss.gnss1PosLla = 1
-binaryOutput1Register.gnss.gnss1PosEcef = 1
-binaryOutput1Register.gnss.gnss1PosUncertainty = 1
-binaryOutput1Register.gnss.gnss1Fix = 1
-binaryOutput1Register.gnss.gnss1NumSats = 1
-binaryOutput1Register.gnss.gnss1TimeUtc = 1
+# binaryOutput1Register.asyncMode.serial2 = 0
+# binaryOutput1Register.time.timeUtc = 1
+# binaryOutput1Register.time.timeStartup = 1
+# binaryOutput1Register.time.timeGps = 1
+# binaryOutput1Register.time.timeSyncIn = 1
+# binaryOutput1Register.time.timeGpsPps = 1
+# binaryOutput1Register.imu.imuStatus = 1
+# binaryOutput1Register.imu.temperature = 1
+# binaryOutput1Register.imu.pressure = 1
+# binaryOutput1Register.imu.accel = 1
+# binaryOutput1Register.imu.mag = 1
+# binaryOutput1Register.attitude.ypr = 1
+# binaryOutput1Register.attitude.quaternion = 1
+# binaryOutput1Register.attitude.dcm = 0
+# binaryOutput1Register.ins.posLla = 1
+# binaryOutput1Register.ins.posEcef = 1
+# binaryOutput1Register.ins.posU = 1
+# binaryOutput1Register.ins.insStatus = 1
+# binaryOutput1Register.gnss.gnss1PosLla = 1
+# binaryOutput1Register.gnss.gnss1PosEcef = 1
+# binaryOutput1Register.gnss.gnss1PosUncertainty = 1
+# binaryOutput1Register.gnss.gnss1Fix = 1
+# binaryOutput1Register.gnss.gnss1NumSats = 1
+# binaryOutput1Register.gnss.gnss1TimeUtc = 1
 s.writeRegister(binaryOutput1Register)
 
 s.disconnect()
