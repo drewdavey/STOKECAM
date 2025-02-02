6050a6edc064ef9c3b877368f1477cf80b35ea86
Drew <drewdavey22@gmail.com>
2025-02-01 18:52:03 -0800

    cc

--- run_cam/VN_test.py
+++ run_cam/VN_test.py
@@ -20,8 +20,8 @@ sync_control.syncOutMode = Registers.SyncControl.SyncOutMode.GpsPps
 s.writeRegister(sync_control)
 
 #### CONFIGURE THE NMEA OUTPUT
-nmea_control = Registers.System.NmeaOutput1()
-nmea_control.port = Registers.System.NmeaOutput1.Port.Serial1
+nmea_control = Registers.NmeaOutput1()
+nmea_control.port = Registers.NmeaOutput1.Port.Serial1
 s.writeRegister(nmea_control)
 
 #### CONFIGURE ADOR AND AODF 
