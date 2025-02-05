77b235a7e8d9eed55367dc1e51ccc017217fcb7b
Drew <drewdavey22@gmail.com>
2025-02-04 17:05:09 -0800

    dd

--- run_cam/utils.py
+++ run_cam/utils.py
@@ -80,11 +80,11 @@ def config_vecnav(portName):
     binaryOutput1Register.time.timeUtc = 1
     binaryOutput1Register.time.timeStartup = 1
     binaryOutput1Register.time.timeGps = 1
-    binaryOutput1Register.imu.imuStatus = 1
-    binaryOutput1Register.imu.temperature = 1
-    binaryOutput1Register.imu.pressure = 1
-    binaryOutput1Register.imu.accel = 1
-    binaryOutput1Register.imu.mag = 0
+    # binaryOutput1Register.imu.imuStatus = 1
+    # binaryOutput1Register.imu.temperature = 1
+    # binaryOutput1Register.imu.pressure = 1
+    # binaryOutput1Register.imu.accel = 1
+    # binaryOutput1Register.imu.mag = 1
     binaryOutput1Register.attitude.ypr = 1
     binaryOutput1Register.attitude.quaternion = 1
     binaryOutput1Register.ins.posLla = 1
@@ -244,11 +244,11 @@ def vecnav_status(portName, fname_log, gps_timeout):
             log.write(f"{tstr}:     timeUtc: {tUtc}\n")
             log.write(f"{tstr}:     timeStartup (μs): {cd.time.timeStartup.microseconds()}\n")
             log.write(f"{tstr}:     timeGps (μs): {cd.time.timeGps.microseconds()}\n")
-            log.write(f"{tstr}:     imuStatus: Accel: {cd.imu.imuStatus.accelStatus}, Gyro: {cd.imu.imuStatus.gyroStatus}, Mag: {cd.imu.imuStatus.magStatus}, PresTemp: {cd.imu.imuStatus.presTempStatus}\n")
-            log.write(f"{tstr}:     temperature: {cd.imu.temperature}\n")
-            log.write(f"{tstr}:     pressure: {cd.imu.pressure}\n")
+            # log.write(f"{tstr}:     imuStatus: Accel: {cd.imu.imuStatus.accelStatus}, Gyro: {cd.imu.imuStatus.gyroStatus}, Mag: {cd.imu.imuStatus.magStatus}, PresTemp: {cd.imu.imuStatus.presTempStatus}\n")
+            # log.write(f"{tstr}:     temperature: {cd.imu.temperature}\n")
+            # log.write(f"{tstr}:     pressure: {cd.imu.pressure}\n")
             # log.write(f"{tstr}:     mag: {cd.imu.mag}\n")
-            log.write(f"{tstr}:     accel: {cd.imu.accel}\n")
+            # log.write(f"{tstr}:     accel: {cd.imu.accel}\n")
             log.write(f"{tstr}:     ypr: {cd.attitude.ypr}\n")
             log.write(f"{tstr}:     quaternion: Scalar: {cd.attitude.quaternion.scalar}, Vector: {cd.attitude.quaternion.vector}\n")
             log.write(f"{tstr}:     gnss1Fix: {cd.gnss.gnss1Fix}\n")
