7c82907a75d552f3bd8f9f7dc5430d4ae6be3465
Drew <drewdavey22@gmail.com>
2025-03-18 15:27:16 -0700

    incorp to test.py

--- run_cam/test.py
+++ run_cam/test.py
@@ -5,7 +5,9 @@
 ##################################
 import sys
 import time
-import vectornav
+import cv2
+import queue
+from collections import deque
 import threading
 import traceback
 from utils import *
@@ -15,6 +17,44 @@ from picamera2 import Picamera2
 from gpiozero import Button, LED
 from vectornav.Plugins import ExporterCsv
 from datetime import datetime, timezone, timedelta
+
+# Circular buffer and queue setup
+buffer_size = 50  # Store last 50 images in memory
+image_buffer0 = deque(maxlen=buffer_size)
+image_buffer1 = deque(maxlen=buffer_size)
+timestamp_buffer = deque(maxlen=buffer_size)
+write_queue = queue.Queue()
+
+def capture_continuous(dt):
+    """Capture images continuously into RAM while the button is held."""
+    while right_button.is_pressed:
+        timestamp = time.monotonic_ns() 
+        img0 = cam0.capture_array("main")  # Capture to NumPy array
+        img1 = cam1.capture_array("main")
+        image_buffer0.append((img0, timestamp))
+        image_buffer1.append((img1, timestamp))
+        timestamp_buffer.append(timestamp)
+        time.sleep(dt)  # Maintain 25Hz capture rate
+
+def write_images_to_sd():
+    """Background process to write images to SD card."""
+    while True:
+        try:
+            img0, img1, timestamp = write_queue.get(timeout=2)
+            filename0 = f"/home/pi/cam0_{timestamp}.jpg"
+            filename1 = f"/home/pi/cam1_{timestamp}.jpg"
+            cv2.imwrite(filename0, img0)  # Save as full-resolution images
+            cv2.imwrite(filename1, img1)
+            print(f"Saved {filename0} and {filename1}")
+        except queue.Empty:
+            break
+
+def process_and_store():
+    """Queue images for writing after button release."""
+    for i in range(len(image_buffer0)):
+        write_queue.put((image_buffer0[i][0], image_buffer1[i][0], image_buffer0[i][1]))
+    thread = threading.Thread(target=write_images_to_sd)
+    thread.start()
 
 def configure_cameras(fname_log, mode):
     global cam0, cam1, config 
@@ -56,15 +96,15 @@ def toggle_modes():
     time.sleep(3)
     [led.off() for led in (red, green, yellow)]
 
-def cap0(fdir_cam0, tnext, i):
-    while time.monotonic_ns() < tnext:
-        pass
-    cam0.capture_array(f"{fdir_cam0}0_{str(time.monotonic_ns())}_{i+1:05}.jpg")
+# def cap0(fdir_cam0, tnext, i):
+#     while time.monotonic_ns() < tnext:
+#         pass
+#     cam0.capture_array(f"{fdir_cam0}0_{str(time.monotonic_ns())}_{i+1:05}.jpg")
 
-def cap1(fdir_cam1, tnext, i):
-    while time.monotonic_ns() < tnext:
-        pass
-    cam1.capture_array(f"{fdir_cam1}1_{str(time.monotonic_ns())}_{i+1:05}.jpg")
+# def cap1(fdir_cam1, tnext, i):
+#     while time.monotonic_ns() < tnext:
+#         pass
+#     cam1.capture_array(f"{fdir_cam1}1_{str(time.monotonic_ns())}_{i+1:05}.jpg")
 
 def exit_standby(fname_log):
     global standby
@@ -89,11 +129,10 @@ def enter_standby(fdir, fname_log, dt, mode, portName):
             while right_button.is_pressed:
                 tnow = time.monotonic_ns()
                 tnext = tnow + int(dt * 1e9)  # Convert seconds to nanoseconds
-                p0 = threading.Thread(target=cap0, args=[fdir_cam0, tnext, i])
-                p1 = threading.Thread(target=cap1, args=[fdir_cam1, tnext, i])
-                p0.start(), p1.start()
-                p0.join(), p1.join()
+                capture_continuous(dt)
+
                 i += 1
+            process_and_store()
             red.off()
         time.sleep(0.2)
     exit_standby(fname_log)
@@ -103,10 +142,7 @@ green = LED(12)                         # Green LED
 yellow = LED(16)                        # Yellow LED
 red = LED(24)                           # Red LED
 right_button = Button(18, hold_time=3)  # Right button
-left_button = Button(17, hold_time=3)   # Left button
-
-portName = '/dev/ttyUSB0'                 # Default port for VN-200
-config_vecnav(portName)                   # Config VN-200 output           
+left_button = Button(17, hold_time=3)   # Left button     
 
 fdir = '/home/drew/testing_drew/'
 fname_log = '/home/drew/testing_drew/log.txt'
