7fecd099c3fe9b28d22094a673ad61c4da07e0ef
Drew <drewdavey22@gmail.com>
2025-04-17 11:45:58 -0700

    dd

--- run_cam/utils.py
+++ run_cam/utils.py
@@ -201,8 +201,8 @@ def config_vecnav(portName):
     binaryOutput1Register.ins.insStatus = 1
     binaryOutput1Register.gnss.gnss1PosLla = 1
     # binaryOutput1Register.gnss.gnss1Dop = 1
-    binaryOutput1Register.gnss.gnss1SatInfo = 1
-    binaryOutput1Register.gnss.gnss1AltMSL = 1
+    # binaryOutput1Register.gnss.gnss1SatInfo = 1
+    # binaryOutput1Register.gnss.gnss1AltMSL = 1
     binaryOutput1Register.gnss.gnss1PosUncertainty = 1
     binaryOutput1Register.gnss.gnss1Fix = 1
     binaryOutput1Register.gnss.gnss1NumSats = 1
